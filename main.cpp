e98510524ddb9dfeddd440573062c49868cd1d05
killianharr <killianh@live.ie>
2023-04-22 11:00:39 +0000

    commit 3

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@ PwmOut led1(LED1);
 AnalogIn pot1(p19);
 int main() {
  while(1) {
-     lcd.printf("%2f\n", pot1.read());
-     lcd.printf("%2e\n", pot1.read());
+     lcd.printf("%.2f\n", pot1.read());
+     lcd.printf("%.2e\n", pot1.read());
  led1 = pot1;
  wait(5);
  }
